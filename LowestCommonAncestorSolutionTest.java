de98f71bfba57bdab639126e2819f173264e4584
tserlis <erlist@tcd.ie>
2017-10-18 19:54:28 +0100

    Added more tests for Node class

--- LowestCommonAncestorSolutionTest.java
+++ LowestCommonAncestorSolutionTest.java
@@ -5,6 +5,28 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LowestCommonAncestorSolutionTest {
+	
+	@Test
+	public void NodeGetDataTest() {
+		Node node = new Node(0);
+		assertEquals(0, node.getData());
+	}
+	
+	@Test
+	public void NodeSetLeftGetLeftTest() {
+		Node node = new Node(0);
+		Node left = new Node(1);
+		node.setLeft(left);
+		assertSame(left, node.getLeft());
+	}
+	
+	@Test
+	public void NodeSetRightGetRightTest() {
+		Node node = new Node(0);
+		Node right = new Node(1);
+		node.setRight(right);
+		assertSame(right, node.getRight());
+	}
 
 	@Test
 	public void BTreeIsEmptyTest() {
